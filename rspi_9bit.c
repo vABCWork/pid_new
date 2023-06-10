889b526137c4a97a9c927a909fb733378fd6d456
vABCWork <97317827+vABCWork@users.noreply.github.com>
2023-06-10 16:34:36 +0900

    Update rspi_9bit.c

--- rspi_9bit.c
+++ rspi_9bit.c
@@ -4,60 +4,60 @@
 #include "rspi_9bit.h"
 
 
-volatile uint8_t spi_sending_fg;	// ���M�� = 1
+volatile uint8_t spi_sending_fg;	// 送信中 = 1
 
 
 
 // RSPI0 SPII0
-// �A�C�h�����荞��
+// アイドル割り込み
 #pragma interrupt (Excep_RSPI0_SPII0(vect=47))
 void Excep_RSPI0_SPII0(void){
 	
-	RSPI0.SPCR.BIT.SPE = 0;         // RSPI�@�\�͖���
-	RSPI0.SPCR2.BIT.SPIIE = 0;	// �A�C�h�����荞�ݗv���̐������֎~
+	RSPI0.SPCR.BIT.SPE = 0;         // RSPI機能は無効
+	RSPI0.SPCR2.BIT.SPIIE = 0;	// アイドル割り込み要求の生成を禁止
 	
-	spi_sending_fg = 0;		// SPI���M���̃t���O�̃N���A
+	spi_sending_fg = 0;		// SPI送信中のフラグのクリア
 }
 
 
 
-// RSPI �|�[�g�̏�����  (LCD�R���g���[���p)
-// �@�}�X�^: �}�C�R��
-//   �X���[�u:  LCD�R���g���[��(ILI9488)
+// RSPI ポートの初期化  (LCDコントローラ用)
+// 　マスタ: マイコン
+//   スレーブ:  LCDコントローラ(ILI9488)
 //
-// PC6 : MOSI     ,SPI_MOSI ,�}�X�^�o�� �X���[�u����   
-// PC5 : RSPCKA   ,SPI_CLK  ,�N���b�N�o��  
-// PC4 : SSLA0    ,DISP_CS  ,�X���[�u�Z���N�g 
-// PC7 : port�o�� ,DISP_RST, LCD�R���g���[��(ILI9488) Reset�p 
+// PC6 : MOSI     ,SPI_MOSI ,マスタ出力 スレーブ入力   
+// PC5 : RSPCKA   ,SPI_CLK  ,クロック出力  
+// PC4 : SSLA0    ,DISP_CS  ,スレーブセレクト 
+// PC7 : port出力 ,DISP_RST, LCDコントローラ(ILI9488) Reset用 
 //
-// ILI9488 DBI Type C Serial Interface Option1 �̏ꍇ�A�R�}���h/�f�[�^���ʂ́A���M�f�[�^�̐擪�r�b�g�Ƃ��Ă���̂ŁA
-// �|�[�g�o�͂ɂ��R�}���h/�f�[�^���ʂ͕K�v�Ȃ��B
-// (PC7 : port�o�� ,DISP_DC ,LCD�R���g���[��(ILI9488)  �R�}���h/�f�[�^���ʗp�@(�R�}���h=low,�f�[�^=high))  
+// ILI9488 DBI Type C Serial Interface Option1 の場合、コマンド/データ判別は、送信データの先頭ビットとしているので、
+// ポート出力によるコマンド/データ判別は必要ない。
+// (PC7 : port出力 ,DISP_DC ,LCDコントローラ(ILI9488)  コマンド/データ判別用　(コマンド=low,データ=high))  
 //
 
 void RSPI_Init_Port(void)
 {
 	
-	MPC.PWPR.BIT.B0WI = 0;  	 // �}���`�t�@���N�V�����s���R���g���[���@�v���e�N�g����
-        MPC.PWPR.BIT.PFSWE = 1;  	// PmnPFS ���C�g�v���e�N�g����
+	MPC.PWPR.BIT.B0WI = 0;  	 // マルチファンクションピンコントローラ　プロテクト解除
+        MPC.PWPR.BIT.PFSWE = 1;  	// PmnPFS ライトプロテクト解除
     
 	MPC.PC6PFS.BYTE = 0x0d;		// PC6 = MOSIA   Master Out slave in
 	MPC.PC5PFS.BYTE = 0x0d;	  	// PC5 = RSPCKA
 	MPC.PC4PFS.BYTE = 0x0d;		// PC4 = SSLA0
 	
-        MPC.PWPR.BYTE = 0x80;      	//  PmnPFS ���C�g�v���e�N�g �ݒ�	
+        MPC.PWPR.BYTE = 0x80;      	//  PmnPFS ライトプロテクト 設定	
 	
-        PORTC.PMR.BIT.B6 = 1;     	// PC6  ���Ӌ@�\�Ƃ��Ďg�p
+        PORTC.PMR.BIT.B6 = 1;     	// PC6  周辺機能として使用
 	PORTC.PMR.BIT.B5 = 1;     	// PC5  
 	PORTC.PMR.BIT.B4 = 1;     	// PC4
 	
-	PORTC.DSCR.BIT.B6 = 1;     	// PC6 ���쓮�o��
+	PORTC.DSCR.BIT.B6 = 1;     	// PC6 高駆動出力
 	PORTC.DSCR.BIT.B5 = 1;     	// PC5  
 	PORTC.DSCR.BIT.B4 = 1;     	// PC4
 	
 	
-	LCD_RESET_PMR  = 0;     	// PC7:�ėp���o�̓|�[�g ,LED�R���g���[�� Reset�p 
-	LCD_RESET_PDR = 1;      	// PC7: �o�̓|�[�g�ɐݒ� LED�R���g���[���ւ�Reset
+	LCD_RESET_PMR  = 0;     	// PC7:汎用入出力ポート ,LEDコントローラ Reset用 
+	LCD_RESET_PDR = 1;      	// PC7: 出力ポートに設定 LEDコントローラへのReset
 	
       
 }
@@ -65,71 +65,71 @@ void RSPI_Init_Port(void)
 
 
 
-// RSPI ���W�X�^�̏����ݒ� 
-//�r�b�g���[�g:
-//  RSPI �R�}���h���W�X�^0 �` 7�iSPCMD0�`7)��BRDV[1:0] �r�b�g��SPBR ���W�X�^�̐ݒ�l(n)�̑g�ݍ��킹�ł��܂�B
+// RSPI レジスタの初期設定 
+//ビットレート:
+//  RSPI コマンドレジスタ0 ～ 7（SPCMD0～7)のBRDV[1:0] ビットとSPBR レジスタの設定値(n)の組み合わせできまる。
 // 
-//  ���݁ARSPI0.SPCMD0��BRDV=0 ,   PCLKB= 32MHz�Ƃ��Ă���B
-//   �r�b�g���[�g = PCLKB / 2*(SPBR+1)
+//  現在、RSPI0.SPCMD0のBRDV=0 ,   PCLKB= 32MHzとしている。
+//   ビットレート = PCLKB / 2*(SPBR+1)
 //   
 //
-//     SPBR = 0 �ŁA32/2 = 16 [MHz]
-//     SPBR = 1 �ŁA32/4 = 8 [MHz]
+//     SPBR = 0 で、32/2 = 16 [MHz]
+//     SPBR = 1 で、32/4 = 8 [MHz]
 //
 
 void RSPI_Init_Reg(void)
 {
 
-	RSPI0.SPCR.BIT.SPMS = 0;        // SPI���� (4����)	
-	RSPI0.SPCR.BIT.TXMD = 1;        // ���M����̂ݍs���B��M�Ȃ�
-	RSPI0.SPCR.BIT.MODFEN = 0;      // ���[�h�t�H���g�G���[���o���֎~
+	RSPI0.SPCR.BIT.SPMS = 0;        // SPI動作 (4線式)	
+	RSPI0.SPCR.BIT.TXMD = 1;        // 送信動作のみ行う。受信なし
+	RSPI0.SPCR.BIT.MODFEN = 0;      // モードフォルトエラー検出を禁止
 	
-	RSPI0.SPCR.BIT.MSTR = 1;	//  �}�X�^���[�h
+	RSPI0.SPCR.BIT.MSTR = 1;	//  マスタモード
 	
 	
-	RSPI0.SPCR.BIT.SPEIE =0;       // �G���[���荞�ݗv���̔������֎~
-	RSPI0.SPCR.BIT.SPTIE =0;       // ���M�o�b�t�@�G���v�e�B���荞�ݗv���̔������֎~
-	RSPI0.SPCR.BIT.SPE = 0;        // RSPI�@�\�͖���
-	RSPI0.SPCR.BIT.SPRIE = 0;      // RSPI��M�o�b�t�@�t�����荞�ݗv���̔������֎~
+	RSPI0.SPCR.BIT.SPEIE =0;       // エラー割り込み要求の発生を禁止
+	RSPI0.SPCR.BIT.SPTIE =0;       // 送信バッファエンプティ割り込み要求の発生を禁止
+	RSPI0.SPCR.BIT.SPE = 0;        // RSPI機能は無効
+	RSPI0.SPCR.BIT.SPRIE = 0;      // RSPI受信バッファフル割り込み要求の発生を禁止
 	
-	RSPI0.SSLP.BYTE = 0;                // �X���[�u�Z���N�g�ɐ� SSL0�`SSL3 �A�N�e�B�uLow
-	RSPI0.SPPCR.BYTE = 0;               //  �ʏ탂�[�h
+	RSPI0.SSLP.BYTE = 0;                // スレーブセレクト極性 SSL0～SSL3 アクティブLow
+	RSPI0.SPPCR.BYTE = 0;               //  通常モード
 	
-	RSPI0.SPSCR.BYTE = 0;               // �]���t�H�[�}�b�g�́A���SPCMD0 ���W�X�^���Q��
+	RSPI0.SPSCR.BYTE = 0;               // 転送フォーマットは、常にSPCMD0 レジスタを参照
 
 	
 	
-	// ILI9488 17.4.2. DBI Type C Option 1 (3-Line SPI System) Timing Characteristics ���ASerial clock cycle (Write) = 66nsec (min)
-	// �������ݎ��ɂ́A15MHz���ō������A16[MHz]�������Ă���B
+	// ILI9488 17.4.2. DBI Type C Option 1 (3-Line SPI System) Timing Characteristics より、Serial clock cycle (Write) = 66nsec (min)
+	// 書き込み時には、15MHzが最高だが、16[MHz]を試している。
 	
 	//RSPI0.SPBR = 0x1;		   // 8 MHz
 	
 	RSPI0.SPBR = 0x0;		   // 16 MHz
 	
-	RSPI0.SPDCR.BIT.SPFC =0x00;    // SPDR ���W�X�^�Ɋi�[�ł���i1 ��̓]���N���j�t���[���� = 1 �t���[�� 
-	RSPI0.SPDCR.BIT.SPRDTD = 0;    // 0�FSPDR�͎�M�o�b�t�@��ǂݏo��
-	RSPI0.SPDCR.BIT.SPLW = 0;      // RSPI�f�[�^���W�X�^(SPDR)�̓��[�h(16bit)�̃A�N�Z�X
-                                       // SPCMD0.BIT.SCKDEN = 1�̏ꍇ�́A
-	RSPI0.SPCKD.BYTE = 0;          // �Z���N�g�M������N���b�N�����܂ł̒x�����ԁ@1RSPCK
-				       // SPCMD0.BIT.SCKDEN = 0�̏ꍇ�A�Z���N�g�M������N���b�N�����܂ł̒x�����Ԃ́A1RSPCK
+	RSPI0.SPDCR.BIT.SPFC =0x00;    // SPDR レジスタに格納できる（1 回の転送起動）フレーム数 = 1 フレーム 
+	RSPI0.SPDCR.BIT.SPRDTD = 0;    // 0：SPDRは受信バッファを読み出す
+	RSPI0.SPDCR.BIT.SPLW = 0;      // RSPIデータレジスタ(SPDR)はワード(16bit)のアクセス
+                                       // SPCMD0.BIT.SCKDEN = 1の場合の、
+	RSPI0.SPCKD.BYTE = 0;          // セレクト信号からクロック発生までの遅延時間　1RSPCK
+				       // SPCMD0.BIT.SCKDEN = 0の場合、セレクト信号からクロック発生までの遅延時間は、1RSPCK
 			
-	RSPI0.SSLND.BYTE = 0;          // SPCMD0.BIT.SLNDEN = 1�̏ꍇ�́A
-	                               // �N���b�N�ŏI�G�b�W����A�Z���N�g�����܂ł̒x������
-	                               // SPCMD0.BIT.SLNDEN = 0�̏ꍇ�A1RSPCK
+	RSPI0.SSLND.BYTE = 0;          // SPCMD0.BIT.SLNDEN = 1の場合の、
+	                               // クロック最終エッジから、セレクト解除までの遅延時間
+	                               // SPCMD0.BIT.SLNDEN = 0の場合、1RSPCK
 				      
-	RSPI0.SPND.BYTE = 0;	       // SPCMD0.BIT.SPNDEN = 1�̏ꍇ�́A�V���A���]���I����́A�Z���N�g�M����A�N�e�B�u���ԁi���A�N�Z�X�x��)�@1RSPCK�{2PCLK	      
-				       // SPCMD0.BIT.SPNDEN = 0�̏ꍇ�A1RSPCK�{2PCLK
+	RSPI0.SPND.BYTE = 0;	       // SPCMD0.BIT.SPNDEN = 1の場合の、シリアル転送終了後の、セレクト信号非アクティブ期間（次アクセス遅延)　1RSPCK＋2PCLK	      
+				       // SPCMD0.BIT.SPNDEN = 0の場合、1RSPCK＋2PCLK
 	
-        RSPI0.SPCR2.BYTE = 0;          // �p���e�B�Ȃ��@�A�C�h�����荞�ݗv���̔������֎~�@�A�C�h�����荞�ݗv���̔������֎~
+        RSPI0.SPCR2.BYTE = 0;          // パリティなし　アイドル割り込み要求の発生を禁止　アイドル割り込み要求の発生を禁止
 
-					// ���M�f�[�^�G���v�e�B���荞��			
-	IR(RSPI0,SPTI0) = 0;		// ���荞�ݗv���̃N���A
-	IEN(RSPI0,SPTI0) = 1;		// ���荞�݋���	
+					// 送信データエンプティ割り込み			
+	IR(RSPI0,SPTI0) = 0;		// 割り込み要求のクリア
+	IEN(RSPI0,SPTI0) = 1;		// 割り込み許可	
 	
-					// �A�C�h�����荞��
-	IPR(RSPI0,SPII0) = 0x06;	// ���荞�݃��x�� = 6�@�@�i15���ō����x��)
-	IR(RSPI0,SPII0) = 0;		// ���荞�ݗv���̃N���A
-	IEN(RSPI0,SPII0) = 1;		// ���荞�݋���				      
+					// アイドル割り込み
+	IPR(RSPI0,SPII0) = 0x06;	// 割り込みレベル = 6　　（15が最高レベル)
+	IR(RSPI0,SPII0) = 0;		// 割り込み要求のクリア
+	IEN(RSPI0,SPII0) = 1;		// 割り込み許可				      
 }
 
 
@@ -137,74 +137,74 @@ void RSPI_Init_Reg(void)
 
 
 
-//     SPCMD 0 �̐ݒ�
+//     SPCMD 0 の設定
 //   
-//     SPI ���[�h3: �N���b�N�A�C�h���� High
-//                :  (�N���b�N�����オ�莞�Ƀf�[�^�T���v��)
+//     SPI モード3: クロックアイドル時 High
+//                :  (クロック立ち上がり時にデータサンプル)
 
-//�@�@�@SSL�M��   : SSLA0
+//　　　SSL信号   : SSLA0
 
 void RSPI_SPCMD_0(void)
 {
 
-					// SPI ���[�h 3 : �\�� OK
-	RSPI0.SPCMD0.BIT.CPOL = 1;	// CPOL= 1 ���p���X(�N���b�N�A�C�h���� High)				
-	RSPI0.SPCMD0.BIT.CPHA = 1;	// CPHA= 1 ��G�b�W�Ńf�[�^�ω��A�����G�b�W�Ńf�[�^�T���v�� (�G�b�W��1���琔����j
+					// SPI モード 3 : 表示 OK
+	RSPI0.SPCMD0.BIT.CPOL = 1;	// CPOL= 1 負パルス(クロックアイドル時 High)				
+	RSPI0.SPCMD0.BIT.CPHA = 1;	// CPHA= 1 奇数エッジでデータ変化、偶数エッジでデータサンプル (エッジは1から数える）
 
 	
-	RSPI0.SPCMD0.BIT.BRDV = 0;	// BRDV= 0 �x�[�X�̃r�b�g���[�g�g�p (�����Ȃ�)
-	RSPI0.SPCMD0.BIT.SSLA = 0;	// SSLA= 0 SSL0���g�p
+	RSPI0.SPCMD0.BIT.BRDV = 0;	// BRDV= 0 ベースのビットレート使用 (分周なし)
+	RSPI0.SPCMD0.BIT.SSLA = 0;	// SSLA= 0 SSL0を使用
 	
-	RSPI0.SPCMD0.BIT.SSLKP = 0;	// �]���I����(���̃v���O�����̏ꍇ�A1byte�]��)�SSSL�M�����l�Q�[�g
+	RSPI0.SPCMD0.BIT.SSLKP = 0;	// 転送終了後(このプログラムの場合、1byte転送)全SSL信号をネゲート
 	
-	RSPI0.SPCMD0.BIT.SPB = 0x08;	//  SPB= 8  ���M �f�[�^�� 9 bit
+	RSPI0.SPCMD0.BIT.SPB = 0x08;	//  SPB= 8  送信 データは 9 bit
 	RSPI0.SPCMD0.BIT.LSBF = 0;	// LSBF= 0  MSB first
 	
-	RSPI0.SPCMD0.BIT.SPNDEN = 0;	// SPNDEN=0 ���A�N�Z�X�x���́A1RSPCK + 2PCLK
-	RSPI0.SPCMD0.BIT.SLNDEN = 0;	// SLNDEN=0 SSL�l�Q�[�g�x���� 1RSPCK
-	RSPI0.SPCMD0.BIT.SCKDEN = 0;	//  SCKDEN=0 RSPCK�x���� 1RSPCK
+	RSPI0.SPCMD0.BIT.SPNDEN = 0;	// SPNDEN=0 次アクセス遅延は、1RSPCK + 2PCLK
+	RSPI0.SPCMD0.BIT.SLNDEN = 0;	// SLNDEN=0 SSLネゲート遅延は 1RSPCK
+	RSPI0.SPCMD0.BIT.SCKDEN = 0;	//  SCKDEN=0 RSPCK遅延は 1RSPCK
 	
 }
 
 
-// LCD�R���g���[��(ILI9488)�ւ̃f�[�^���M �@(���M���荞�݊J�n)
+// LCDコントローラ(ILI9488)へのデータ送信 　(送信割り込み開始)
 //  
-//  ����:    sd_num :���M�f�[�^�o�C�g�� 
-//          *data_pt: ���M�f�[�^�̊i�[�ʒu
-//�@�@�@�@�@cmd_flg : 1=�R�}���h�A�p�����[�^���M�̏ꍇ
-//                    0=�\���f�[�^���M�̏ꍇ
+//  入力:    sd_num :送信データ数 
+//          *data_pt: 送信データの格納位置
+//　　　　　cmd_flg : 1=コマンド、パラメータ送信の場合
+//                    0=表示データ送信の場合
 //
 
 
 void rspi_data_send( uint32_t sd_num, uint16_t *data_pt )
 {
 	
-	DMAC2.DMSAR = data_pt;	 		 // �]�����A�h���X		
-	DMAC2.DMDAR = (void *)&RSPI0.SPDR.WORD;	 // �]����A�h���X 
+	DMAC2.DMSAR = data_pt;	 		 // 転送元アドレス		
+	DMAC2.DMDAR = (void *)&RSPI0.SPDR.WORD;	 // 転送先アドレス 
 	
-	DMAC2.DMCRA = sd_num; 	 	// �]���� (���M�o�C�g��)	
+	DMAC2.DMCRA = sd_num; 	 	// 転送回数	
 	    
-	DMAC2.DMCNT.BIT.DTE = 1;    // DMAC2 (DMAC �`�����l��2) �]������
+	DMAC2.DMCNT.BIT.DTE = 1;    // DMAC2 (DMAC チャンネル2) 転送許可
 	
 	
-	spi_sending_fg = 1;			// SPI���M���̃t���O�Z�b�g
+	spi_sending_fg = 1;			// SPI送信中のフラグセット
 	
-        RSPI0.SPCR2.BIT.SPIIE = 0;		// �A�C�h�����荞��(SPII)�̋֎~
-	RSPI0.SPCR.BIT.SPTIE = 1;       	// ���M�o�b�t�@�G���v�e�B���荞�ݗv���̔���������
-						// (���M�J�n���̑��M�o�b�t�@�G���v�e�B���荞�ݗv���́ASPTIE �r�b�g�Ɠ����܂��͌�ɁASPE �r�b�g���g1�h �ɂ��邱�ƂŔ������܂��B(page 1010))
-	RSPI0.SPCR.BIT.SPE = 1;        		// RSPI �@�\�L��
+        RSPI0.SPCR2.BIT.SPIIE = 0;		// アイドル割り込み(SPII)の禁止
+	RSPI0.SPCR.BIT.SPTIE = 1;       	// 送信バッファエンプティ割り込み要求の発生を許可
+						// (送信開始時の送信バッファエンプティ割り込み要求は、SPTIE ビットと同時または後に、SPE ビットを“1” にすることで発生します。(page 1010))
+	RSPI0.SPCR.BIT.SPE = 1;        		// RSPI 機能有効
 
-	rspi_data_send_wait();			// �S�f�[�^�̑��M�����҂�
+	rspi_data_send_wait();			// 全データの送信完了待ち
 	
 	
 }
 
 //
-// �S�f�[�^�̑��M�����҂�
+// 全データの送信完了待ち
 //
 void  rspi_data_send_wait(void)
 {
-	while( spi_sending_fg == 1) {		// �A�����đ��M����ꍇ�́A���M�����҂�
+	while( spi_sending_fg == 1) {		// 連続して送信する場合の、送信完了待ち
 	
 	}
 	
